055c3ea2c513779e49544573e6f4da2936c038d6
Sam Stratter <humanharddrive@gmail.com>
2024-05-15 22:45:36 -0400

    Functioning wireless

--- Code/Adapter/WirelessComm.cpp
+++ Code/Adapter/WirelessComm.cpp
@@ -1,6 +1,8 @@
 #include "WirelessComm.h"
 #include "PinDefs.h"
 
+#include "Utils.h"
+
 WirelessComm::WirelessComm()
 {
 }
@@ -27,9 +29,23 @@ void WirelessComm::Init()
     if(!m_Radio.begin(&m_SPIBus, NRF_CE_PIN, NRF_CS_PIN))
         printf("Failed to connect to radio\n");
 
-    m_Radio.openWritingPipe(0x10101010);
-    m_Radio.openReadingPipe(1, 0x01010101);
+    if(GetInterfaceType() == CONSOLE_SIDE_INTERFACE)
+    {
+        m_Radio.openWritingPipe(0x10101010);
+        m_Radio.openReadingPipe(1, 0x01010101);
+    }
+    else
+    {
+        m_Radio.openWritingPipe(0x01010101);
+        m_Radio.openReadingPipe(1, 0x10101010);
+    }
+
     m_Radio.setAutoAck(false);
+
+    m_Radio.txDelay = 0; //Autoack is disabled
+    m_Radio.csDelay = 1;
+
+    m_Radio.setDataRate(rf24_datarate_e::RF24_2MBPS);
 
     m_Radio.startListening();
 }
@@ -67,6 +83,16 @@ bool WirelessComm::Write(void *pBuf, uint8_t len)
 
 uint8_t WirelessComm::Read(void *pBuf, uint8_t len)
 {
+    if(m_Radio.available())
+    {
+        gpio_put(RX_ACTIVITY_PIN, true);
+
+        m_Radio.read(pBuf, len);
+
+        gpio_put(RX_ACTIVITY_PIN, false);
+        return len;
+    }
+
     return 0;
 }
 
