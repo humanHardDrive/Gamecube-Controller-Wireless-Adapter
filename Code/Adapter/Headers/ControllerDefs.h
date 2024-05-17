860a55a594148c2de2274402de7e202be06f6d66
Sam Stratter <humanharddrive@gmail.com>
2024-05-16 21:50:19 -0400

    Rework controller definitions

--- Code/Adapter/Headers/ControllerDefs.h
+++ Code/Adapter/Headers/ControllerDefs.h
@@ -7,31 +7,32 @@ const uint32_t POLL_RUMBLE_CMD = 0b010000000000001100000011;
 
 typedef struct
 {
-    //Buttons
-    uint8_t pad0 : 3;
-    uint8_t StartBtn : 1;
-    uint8_t YBtn : 1;
-    uint8_t XBtn : 1;
-    uint8_t BBtn : 1;
-    uint8_t ABtn : 1;
-    uint8_t pad1 : 1;
-    uint8_t LBtn : 1;
-    uint8_t RBtn : 1;
-    uint8_t ZBtn : 1;
-    uint8_t UpBtn : 1;
-    uint8_t DownBtn : 1;
-    uint8_t RightBtn : 1;
-    uint8_t LeftBtn : 1;
+    //Analog triggers
+    uint8_t RVal;
+    uint8_t LVal;
 
     //Joysticks
-    int8_t JoyX;
-    int8_t JoyY;
-    int8_t CX;
-    int8_t CY;
-    
-    //Analog triggers
-    uint8_t LVal;
-    uint8_t RVal;
+    uint8_t CY;
+    uint8_t CX;
+    uint8_t JoyY;
+    uint8_t JoyX;
+
+    //D-Pad
+    uint8_t DLeft : 1;
+    uint8_t DRight : 1;
+    uint8_t DDown : 1;
+    uint8_t DUp : 1;
+
+    //Buttons
+    uint8_t ZBtn : 1;
+    uint8_t RBtn : 1;
+    uint8_t LBtn : 1;
+    uint8_t pad : 1;
+    uint8_t ABtn : 1;
+    uint8_t BBtn : 1;
+    uint8_t XBtn : 1;
+    uint8_t YBtn : 1;
+    uint8_t StartBtn : 1;
 }ControllerValues;
 
 typedef struct
